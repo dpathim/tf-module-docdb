fbaa694604caed28a94586a84f4cc71f2dbc5ff5
Deepthi <dpdevops6@gmail.com>
2024-03-04 16:27:17 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -4,3 +4,14 @@ resource "aws_docdb_subnet_group" "main" {
 
   tags =  merge(local.tags, {Name = "${local.name_prefix}-subnet-group" })
   }
+
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${local.name_prefix}-subnet-group"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
